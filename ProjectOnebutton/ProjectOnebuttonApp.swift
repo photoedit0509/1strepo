0dc5df06b60253a9314114c8c1a068f2548e9d1d
photoedit05 <photoedit0509@gmail.com>
2026-06-15 08:28:10 -0500

    added new files

---
+++ ProjectOnebutton/ProjectOnebuttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOnebuttonApp.swift
+//  ProjectOnebutton
+//
+//  Created by Arjun_Aarna on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOnebuttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
